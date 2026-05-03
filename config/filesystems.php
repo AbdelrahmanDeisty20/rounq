fedb5c3a73e28ccd47fab34616e7ca9d012a4b76
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-05-03 18:51:59 +0300

    feat: add filesystem configuration for local, public, and s3 storage disks

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'root' => public_path('storage'),
+            'url' => rtrim(env('APP_URL', 'https://movesaudii.com'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
